6dbbe9ff49b0ae549c49538ab3bc3d605621bf7a
Hengxi Zhang <72500446+Dylan2020THU@users.noreply.github.com>
2022-05-16 15:09:36 +0800

    Update and rename ray_remote.py to ray_remote_def.py

--- ray_remote_def.py
+++ ray_remote_def.py
@@ -1,5 +1,5 @@
 """
-Goal: How to use ray architecture
+Goal: How to use ray architecture: def
 Born time: 2022.5.16
 Latest update: 2022.5.16
 Creator: ZHX
@@ -26,4 +26,7 @@ def add(a, b):
 id1 = read_array.remote(file=[1, 2, 3])
 id2 = read_array.remote([4, 5, 6])
 id3 = add.remote(id1, id2)  # this will be visited after id1 and id2
-ray.get(id3) # To fetch the output
+ray.get(id1)
+ray.get(id2)
+print(ray.get(id3))
+
